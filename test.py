ae97e3090c71f33750f77fdbe8e80f96df4c5772
Ibai Ros Iturgaiz <ibairos@amazon.com>
2021-04-01 11:39:53 +0200

    Implemented the race scraper

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import scraper
 
 # Examples
 #scraper.scrap_teams_by_year(2021)
-#scraper.scrap_stage('tour-de-france', 2021, 1)
-#scraper.scrap_stage_gc('tour-de-france', 2021, 1)
+#scraper.scrap_stage('tour-de-france', 2020, 2)
+#scraper.scrap_stage_gc('tour-de-france', 2020, 2)
 #scraper.scrap_final_gc('tour-de-france', 2021)
 #scraper.scrap_final_points('tour-de-france', 2021)
 #scraper.scrap_final_kom('tour-de-france', 2021)
