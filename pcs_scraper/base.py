4d62bb8ca6729c0c51c0903c42474daede4289ed
Ibai Ros <ibai.ros@hotmail.com>
2022-04-03 14:16:50 +0200

    Fix chromedriver initialization

--- pcs_scraper/base.py
+++ pcs_scraper/base.py
@@ -4,10 +4,11 @@ import os
 
 
 def init_driver() -> webdriver:
-    print("Initializing Webdriver...")
     if 'MAC' not in os.environ:
+        print("Initializing display")
         display = Display(visible=False, size=(1200, 1200))
         display.start()
+        print("Display initialized")
 
     # Create a new Chrome session
     chrome_options = webdriver.ChromeOptions()
@@ -16,14 +17,10 @@ def init_driver() -> webdriver:
     chrome_options.add_argument('--disable-dev-shm-usage')
     prefs = {"profile.managed_default_content_settings.images": 2}
     chrome_options.add_experimental_option("prefs", prefs)
-    if 'DRIVER' not in os.environ:
-        chrome_options.binary_location = '/usr/bin/chromium-browser'
-        driver = webdriver.Chrome(chrome_options=chrome_options,
-                                  executable_path='/usr/lib/chromium-browser/chromedriver')
-    else:
-        driver = webdriver.Chrome(chrome_options=chrome_options)
+    print("Initializing Chromedriver")
+    driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(30)
     driver.maximize_window()
+    print("Chromedriver initialized")
 
-    print("Webdriver initialized!")
     return driver
